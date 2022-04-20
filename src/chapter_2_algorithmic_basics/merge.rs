53f255a87729f00e65927849629def5b715df4a8
g <gxd2013@outlook.com>
2022-04-20 10:01:25 +0800

    归并排序，添加递归部分

--- src/chapter_2_algorithmic_basics/merge.rs
+++ src/chapter_2_algorithmic_basics/merge.rs
@@ -1,5 +1,17 @@
 //! 归并排序。
 
+/// 排序。
+fn merge_sort(input: &mut [i32]) {
+    if input.len() < 2 {
+        return;
+    }
+
+    let mid = input.len() / 2;
+    merge_sort(&mut input[..mid]);
+    merge_sort(&mut input[mid..]);
+    sort(input, 0, mid, input.len());
+}
+
 /// 假定：lo..min、min..hi 的部分已经排序。
 fn sort(input: &mut [i32], lo: usize, mid: usize, hi: usize) {
     let mut left = input[lo..mid].to_vec();
@@ -22,16 +34,24 @@ fn sort(input: &mut [i32], lo: usize, mid: usize, hi: usize) {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
-    use crate::chapter_2_algorithmic_basics::{merge::sort, is_sorted};
+    use crate::chapter_2_algorithmic_basics::{is_sorted, merge::*};
 
     #[test]
-    fn test() {
+    fn sort_test() {
         let mut buf = [2, 3, 5, 1, 4, 6, 9];
         let len = buf.len();
         sort(&mut buf, 0, len / 2, len);
+
+        println!("{buf:?}");
+        assert!(is_sorted(&buf));
+    }
+
+    #[test]
+    fn merge_test() {
+        let mut buf = [3, 2, 5, 1, 9, 6, 4];
+        merge_sort(&mut buf);
 
         println!("{buf:?}");
         assert!(is_sorted(&buf));
