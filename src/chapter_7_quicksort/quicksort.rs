7c3f47e32c6642ab80ce44f953cfa0c90a779792
gxd <togxd@outlook.com>
2022-04-23 10:28:25 +0800

    添加模拟尾递归的快速排序算法

--- src/chapter_7_quicksort/quicksort.rs
+++ src/chapter_7_quicksort/quicksort.rs
@@ -11,6 +11,18 @@ fn sort(a: &mut [i32]) {
     sort(&mut a[q + 1..]);
 }
 
+/// 尾递归版排序。
+fn tail_recursive_sort(a: &mut [i32]) {
+    let mut low = 0;
+    // 确保每次可划分的切片长度大于 1。
+    while low + 1 < a.len() {
+        let mid = partition(&mut a[low..]);
+
+        tail_recursive_sort(&mut a[low..mid]);
+        low = mid + 1;
+    }
+}
+
 /// 将切片划分为两部分，返回主元索引。
 fn partition(a: &mut [i32]) -> usize {
     // 最后一个元素作为主元，应排除。
@@ -43,6 +55,13 @@ mod tests {
     fn sort_test() {
         let mut buf = [4, 9, 3, 5, 6];
         sort(&mut buf);
+        assert_eq!(&[3, 4, 5, 6, 9], &buf);
+    }
+
+    #[test]
+    fn tail_recursive_sort_test() {
+        let mut buf = [4, 9, 3, 5, 6];
+        tail_recursive_sort(&mut buf);
         assert_eq!(&[3, 4, 5, 6, 9], &buf);
     }
 }
