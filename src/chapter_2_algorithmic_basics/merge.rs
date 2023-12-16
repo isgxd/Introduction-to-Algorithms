d30fc0ded5aaad405d90d3f6955f0a04568a76e1
g <togxd@outlook.com>
2023-12-16 15:18:02 +0800

    重构归并排序的归并函数名，修复注释错误

--- src/chapter_2_algorithmic_basics/merge.rs
+++ src/chapter_2_algorithmic_basics/merge.rs
@@ -9,11 +9,11 @@ fn merge_sort(input: &mut [i32]) {
     let mid = input.len() / 2;
     merge_sort(&mut input[..mid]);
     merge_sort(&mut input[mid..]);
-    sort(input, 0, mid, input.len());
+    merge(input, 0, mid, input.len());
 }
 
-/// 假定：lo..min、min..hi 的部分已经排序。
-fn sort(input: &mut [i32], lo: usize, mid: usize, hi: usize) {
+/// 假定：lo..mid、mid..hi 的部分已经排序。
+fn merge(input: &mut [i32], lo: usize, mid: usize, hi: usize) {
     let mut left = input[lo..mid].to_vec();
     let mut right = input[mid..hi].to_vec();
 
@@ -42,7 +42,7 @@ mod tests {
     fn sort_test() {
         let mut buf = [2, 3, 5, 1, 4, 6, 9];
         let len = buf.len();
-        sort(&mut buf, 0, len / 2, len);
+        merge(&mut buf, 0, len / 2, len);
 
         println!("{buf:?}");
         assert!(is_sorted(&buf));
